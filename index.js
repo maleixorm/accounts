3bcf9275261d9208004ead3c27740c0f48b547c7
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-13 19:24:22 -0300

    Criando a função operation, para gerar o menu da aplicação

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 // módulos externos
-const inquirer = require('inquirer');
-const chalk = require('chalk');
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
 // módulos internos
-const fs = require('fs');
+const fs = require("fs");
+const { type } = require("os");
 
 console.log("Iniciamos o Accounts");
+
+operation();
+
+function operation() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: [
+          "Criar Conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) => {
+        const action = answer['action'];
+        console.log(action);
+    })
+    .catch((err) => console.log(err));
+}
