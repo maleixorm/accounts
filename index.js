b2f40e228626c754dee7ec9a954ee0d7d84af75f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-14 10:37:26 -0300

    Adicionando a função de criar contas

--- index.js
+++ index.js
@@ -27,17 +27,31 @@ function operation() {
       },
     ])
     .then((answer) => {
-        const action = answer['action'];
-        if (action === 'Criar Conta') {
-            createAccount();
-        }
+      const action = answer["action"];
+      if (action === "Criar Conta") {
+        createAccount();
+      }
     })
     .catch((err) => console.log(err));
 }
 
 // Create an account
 function createAccount() {
-    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
-    console.log(chalk.green('Defina as opções da sua conta a seguir:'));
+  console.log(chalk.bgGreen.black("Parabéns por escolher o nosso banco!"));
+  console.log(chalk.green("Defina as opções da sua conta a seguir:"));
+  buildAccount();
+}
 
+function buildAccount() {
+  inquirer
+    .prompt([
+      {
+        name: "accountName",
+        message: "Digite um nome para a sua conta:",
+      },
+    ])
+    .then((answer) => {
+      console.log(answer);
+    })
+    .catch((err) => console.log(err));
 }
